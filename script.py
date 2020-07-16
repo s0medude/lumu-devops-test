5d206b68f4ba774522f62866dee8a06e9f556b6b
s0medude <andresnikolas@gmail.com>
2020-07-15 22:20:17 -0500

    add find_directory method for search by the directory name

--- script.py
+++ script.py
@@ -1,16 +1,31 @@
 import os
+import re
 import operator
 from os.path import join, getsize
 
 # Search for the given path and returned it if exists or return False if it is not present
 def is_directory(path):
     if os.path.isdir(path) and os.path.exists(path):    
-        print('The {} is a existing path and is a directory'.format(path))
+        print('The {} is an existing directory!'.format(path))
         return path
     else:
         print('The {} is not a directory'.format(path))
-        print('Try again! \n')
+        print('Try again!')
         return False
+
+# Find the directory name by executing a bash command with the name as a parameter,
+# then check if the resulting path is an existing directory
+def find_directory(name):
+    command = 'find ~/ -type d -name {}'.format(name)
+    p = os.popen(command).read()
+    if p:
+        name = re.sub('\n', '', p)
+        path = is_directory(name)
+        return path
+    else: 
+        print("\nThere's not a directory such as: {} ".format(name))
+        return False
+
 
 # Return a list of the files for the given directory
 def get_files_from_directory(directory):
@@ -67,13 +82,26 @@ def get_average_size(files):
 
 # Initializa the script 
 def initialize():
-    path = None
-    while path == False or path is None:
-        input_path = input('Please enter the full path of the directory to be analyze: \n')
-        path = is_directory(input_path)
-        print('\n')
+    directory, choice = None, 0 
 
-    files           = get_files_from_directory(path)
+
+    while choice == 0 or choice > 2:
+        choice = int(input('Press the number option:\n1. Directory Full Path\n2, Directory Name\n'))
+    else:       
+        if choice == 1:       
+            while not directory:
+                input_path = input('Please enter the full path of the directory to be analyze: \n')
+                print('\n')
+                directory = is_directory(input_path)
+                
+        elif choice == 2:
+            while not directory:
+                input_path = input('Please enter the name of the directory to be analyze: \n')
+                print('\n')
+                directory = find_directory(input_path)
+                
+
+    files           = get_files_from_directory(directory)
     total_files     = get_files_count(files)
     total_size      = get_total_size(files)
     average_size    = get_average_size(files)
@@ -84,9 +112,10 @@ def initialize():
     print(f'Total size: {total_size}', end='    ')
     print(f'Average file size: {average_size}')
     print('Top Files: ')
-    for key, value in top_files.items():
-        print(f'{key} ', end='==> ')
-        print(f'{value} kb')
+    for file, size in top_files.items():
+        file = os.path.basename(file)
+        print(f'{file} ', end='==> ')
+        print(f'{size} kb')
         
 
 
